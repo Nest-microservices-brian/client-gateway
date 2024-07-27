1c18a89d21665be936950ed2bb1e89970ac3a121
Brian Alvarez <brianalvarez2000@gmail.com>
2024-07-27 17:07:44 -0500

    custom rpc exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envVars } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
+
+  await app.listen(envVars.PORT);
+
+  logger.log(`Gateway running on PORT ${envVars.PORT}`);
 }
 bootstrap();
---
+++ src/products/products.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct() {
+    return this.productsClient.send({ cmd: 'create_product' }, {});
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find_all_products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async findProductById(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+    // try {
+    //   const product = await firstValueFrom(
+    //     this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+    //   );
+    //   return product;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return `Esta funcion elimina el producto con id ${id}`;
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return `Esta funcion actualiza el producto con id ${id} ${body}`;
+  }
+}
