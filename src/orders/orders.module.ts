1f97f6da487e9af3601d74a336b4cb66e6339909
Brian Alvarez <brianalvarez2000@gmail.com>
2024-07-27 21:16:10 -0500

    config comunication orders microservice

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envVars, ORDER_SERVICE } from 'src/config';
+
+@Module({
+  controllers: [OrdersController],
+  providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: ORDER_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envVars.ORDERS_MICROSERVICE_HOST,
+          port: envVars.ORDERS_MICROSERVICE_PORT,
+        },
+      },
+    ]),
+  ],
+})
+export class OrdersModule {}
